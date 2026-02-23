223fd7e3a8a9cca97d644c3b5a964a87ca714370
Kenneth <kenneth@hypeworkz.com>
2026-02-23 13:38:53 +0800

    add nodejs server

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  fs.readFile('./index.html', (err, data) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
